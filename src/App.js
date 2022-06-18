203d7948bac52f196971fd4568f7bfe1fb2d765d
unknown <cesyrub@gmail.com>
2022-06-18 16:53:05 +0200

    Start files

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/Home';
+import Error from './pages/Error';
+import NotFound from './pages/NotFound';
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+
+        <Route path="/error" element={<Error />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
