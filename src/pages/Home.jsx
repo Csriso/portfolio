280bb5ebbc08fbd1996b396582bf139bcdf54325
unknown <cesyrub@gmail.com>
2022-06-30 10:18:39 +0200

    line touches

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -48,7 +48,7 @@ export default function Home() {
             <span className='line-one z-10' ref={lineOneRef}></span>
             <span className='line-two z-10' ref={lineTwoRef}></span>
             <div className='w-full flex flex-col lg:flex-row justify-center items-center place-items-center content-center bg-[#001D3D] h-full'>
-                <div className="flex flex-col justify-center lg:items-end items-center place-items-center content-center lg:w-1/2 w-full h-1/2">
+                <div className="flex flex-col justify-center lg:items-center items-center place-items-center content-center lg:w-1/2 w-full h-1/2">
                     <div className="title-container flex flex-col justify-center lg:items-start items-center place-items-start content-center">
                         <h1 className='cesar-title lg:text-8xl text-7xl' ref={cesarRef} style={{ opacity: 0 }}>César Iriso</h1>
                         <h2 className='sub-title lg:text-5xl text-5xl'>Full Stack Web Developer<span className='line-animation font-serif self-start place-self-start' ref={lineRef}>|</span></h2>
